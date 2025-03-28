9ee13e1f6bbf505f38e58de910a75070570a793c
arunp <arunbear@cpan.org>
2025-03-28 16:47:20 +0000

    Improve formatting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,16 @@
 import javax.swing.SwingUtilities;
+
 import ihm.HomeWindow;
 
+public class Main implements Runnable {
 
-public class Main implements Runnable  {
-	
-	    public static void main(String[] args) {
-    	SwingUtilities.invokeLater(new Main());
-
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Main());
     }
 
-
-
-	@Override
-	public void run() {
+    @Override
+    public void run() {
         new HomeWindow();
-
     }
 
 }
