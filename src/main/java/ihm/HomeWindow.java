3d2fbe89bebeff9f42d2f315badbc66adf3bea6b
arunp <arunbear@cpan.org>
2025-05-01 14:17:58 +0100

    Fix whitespace issues

--- src/main/java/ihm/HomeWindow.java
+++ src/main/java/ihm/HomeWindow.java
@@ -10,25 +10,25 @@ import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
 public class HomeWindow {
-	public HomeWindow() {
-	 	JFrame frame = new JFrame("Sokoban v1.0 par Gabriel FARAGO");
-	 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	 	frame.setResizable(false);
+    public HomeWindow() {
+        JFrame frame = new JFrame("Sokoban v1.0 par Gabriel FARAGO");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
 
         JLabel title = new JLabel("SOKOBAN", SwingConstants.CENTER);
-		title.setFont(new Font(Font.SERIF, Font.BOLD, 70));
-	 	title.setBounds(0, 50, 400, 100);
-	 	
-	    JButton play = new JButton("Play");
-	    JButton edit = new JButton("Edit levels");
-	    JButton quit = new JButton("Quit");
-	    
-	    play.setBounds(75,225,250,50);  
-	    edit.setBounds(25,300,150,50);
-	    quit.setBounds(225, 300, 150, 50);
-	    
-	    quit.addActionListener(_ -> System.exit( 0 ));
-	    play.addActionListener(_ -> {
+        title.setFont(new Font(Font.SERIF, Font.BOLD, 70));
+        title.setBounds(0, 50, 400, 100);
+
+        JButton play = new JButton("Play");
+        JButton edit = new JButton("Edit levels");
+        JButton quit = new JButton("Quit");
+
+        play.setBounds(75, 225, 250, 50);
+        edit.setBounds(25, 300, 150, 50);
+        quit.setBounds(225, 300, 150, 50);
+
+        quit.addActionListener(_ -> System.exit(0));
+        play.addActionListener(_ -> {
             frame.dispose();
             try {
                 new LevelSelection();
@@ -37,7 +37,7 @@ public class HomeWindow {
                 e1.printStackTrace();
             }
         });
-	    edit.addActionListener(_ -> {
+        edit.addActionListener(_ -> {
             frame.dispose();
             try {
                 new LevelEditorSetup();
@@ -46,16 +46,16 @@ public class HomeWindow {
                 e1.printStackTrace();
             }
         });
-	    
-	    frame.add(play);
-	    frame.add(edit);
-	    frame.add(quit);
-	    frame.add(title);
-	    frame.setSize(400,400);
-	    frame.setLayout(null);
-		frame.setLocationRelativeTo(null); // center the window
-	    frame.setVisible(true);
 
-	}
+        frame.add(play);
+        frame.add(edit);
+        frame.add(quit);
+        frame.add(title);
+        frame.setSize(400, 400);
+        frame.setLayout(null);
+        frame.setLocationRelativeTo(null); // center the window
+        frame.setVisible(true);
+
+    }
 }
 
