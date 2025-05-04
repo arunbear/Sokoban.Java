68952ed145439ed4d3e31e2a2364e25476a28b9d
arunp <arunbear@cpan.org>
2025-05-04 16:51:51 +0100

    Improve variable names

--- src/main/java/ihm/LevelSelection.java
+++ src/main/java/ihm/LevelSelection.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.stream.IntStream;
 
 import javax.swing.Icon;
@@ -21,26 +20,26 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import logic.Controller;
 
 public class LevelSelection {
-    private static File path_selected;
+    private static File selectedLevelFile;
 
     public LevelSelection() throws IOException {
 
-        path_selected = new File(new File(".").getCanonicalPath() + "/levels/level1.txt");
+        selectedLevelFile = new File(new File(".").getCanonicalPath() + "/levels/level1.txt");
 
         JFrame levelSelection = new JFrame("Sokoban v1.0 par Gabriel FARAGO");
         levelSelection.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JLabel path = new JLabel("level1.txt", SwingConstants.CENTER);
-        path.setBounds(50, 150, 300, 50);
+        JLabel levelFileLabel = new JLabel("level1.txt", SwingConstants.CENTER);
+        levelFileLabel.setBounds(50, 150, 300, 50);
 
-        JButton browse = createBrowseButton(path);
+        JButton browse = createBrowseButton(levelFileLabel);
 
         JLabel title = createTitle();
         JButton quit = createQuitButton();
         JButton back = createBackButton(levelSelection);
         JButton play = createPlayButton(levelSelection);
 
-        JComboBox<String> levelList = createLevelList(path);
+        JComboBox<String> levelList = createLevelList(levelFileLabel);
 
         levelSelection.add(play);
         levelSelection.add(back);
@@ -48,7 +47,7 @@ public class LevelSelection {
         levelSelection.add(title);
         levelSelection.add(levelList);
         levelSelection.add(browse);
-        levelSelection.add(path);
+        levelSelection.add(levelFileLabel);
         levelSelection.setSize(400, 400);
         levelSelection.setLayout(null);
         levelSelection.setLocationRelativeTo(null);
@@ -82,7 +81,7 @@ public class LevelSelection {
                 if (returnVal == JFileChooser.APPROVE_OPTION) {
                     String browser_result = fileBrowser.getSelectedFile().getName();
                     path.setText(browser_result);
-                    path_selected = fileBrowser.getSelectedFile();
+                    selectedLevelFile = fileBrowser.getSelectedFile();
                 }
 
             }
@@ -104,7 +103,7 @@ public class LevelSelection {
             var fileName = "level%d.txt".formatted(levelList.getSelectedIndex() + 1);
             path.setText(fileName);
             try {
-                path_selected = new File(new File(".").getCanonicalPath() + "/levels/%s".formatted(fileName));
+                selectedLevelFile = new File(new File(".").getCanonicalPath() + "/levels/%s".formatted(fileName));
             } catch (IOException e1) {
                 e1.printStackTrace();
             }
@@ -119,7 +118,7 @@ public class LevelSelection {
         play.addActionListener(e -> {
             levelSelection.dispose();
             try {
-                new SokobanWindow(new Controller(path_selected.getPath()));
+                new SokobanWindow(new Controller(selectedLevelFile.getPath()));
             } catch (IOException e1) {
                 // TODO Auto-generated catch block
                 e1.printStackTrace();
