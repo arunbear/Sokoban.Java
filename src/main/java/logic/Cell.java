29d0d85a1ce4cc43786018ef0ad1eafef26e4925
Arun Prasaad <arunbear@cpan.org>
2025-09-28 15:14:30 +0100

    Improve naming

--- src/main/java/logic/Cell.java
+++ src/main/java/logic/Cell.java
@@ -24,16 +24,16 @@ public class Cell {
     	this.tileType = tileType;
     }
 
-    public void setAdjacentCellContent(Direction direction, TileType content) {
+    public void setAdjacentCellType(Direction direction, TileType type) {
         switch (direction) {
-            case UP    -> warehouse.getCell(line - 1, column).setTileType(content);
-            case DOWN  -> warehouse.getCell(line + 1, column).setTileType(content);
-            case LEFT  -> warehouse.getCell(line, column - 1).setTileType(content);
-            case RIGHT -> warehouse.getCell(line, column + 1).setTileType(content);
+            case UP    -> warehouse.getCell(line - 1, column).setTileType(type);
+            case DOWN  -> warehouse.getCell(line + 1, column).setTileType(type);
+            case LEFT  -> warehouse.getCell(line, column - 1).setTileType(type);
+            case RIGHT -> warehouse.getCell(line, column + 1).setTileType(type);
         }
     }
 
-    public TileType getAdjacentCellContent(Direction direction) {
+    public TileType getAdjacentCellType(Direction direction) {
         return switch (direction) {
             case UP    -> warehouse.getCell(line - 1, column).getTileType();
             case DOWN  -> warehouse.getCell(line + 1, column).getTileType();
@@ -51,17 +51,17 @@ public class Cell {
                 if (boxCellCannotAcceptWorker(direction)) {
                     return false;
                 }
-                switch (this.getAdjacentCellContent(direction)) {
+                switch (this.getAdjacentCellType(direction)) {
                     case UNSTORED_BOX, STORED_BOX, WALL, OUTSIDE -> {
                         return false;
                     }
                     case STORAGE_AREA -> {
                         this.setTileType(TileType.WORKER_ON_FLOOR);
-                        this.setAdjacentCellContent(direction, TileType.STORED_BOX);
+                        this.setAdjacentCellType(direction, TileType.STORED_BOX);
                     }
                     case FLOOR -> {
                         this.setTileType(TileType.WORKER_ON_FLOOR);
-                        this.setAdjacentCellContent(direction, TileType.UNSTORED_BOX);
+                        this.setAdjacentCellType(direction, TileType.UNSTORED_BOX);
                     }
                     default -> {
                     }
@@ -71,17 +71,17 @@ public class Cell {
                 if (boxCellCannotAcceptWorker(direction)) {
                     return false;
                 }
-                switch (this.getAdjacentCellContent(direction)) {
+                switch (this.getAdjacentCellType(direction)) {
                     case UNSTORED_BOX, WALL, STORED_BOX -> {
                         return false;
                     }
                     case STORAGE_AREA -> {
                         this.setTileType(TileType.WORKER_IN_STORAGE_AREA);
-                        this.setAdjacentCellContent(direction, TileType.STORED_BOX);
+                        this.setAdjacentCellType(direction, TileType.STORED_BOX);
                     }
                     case FLOOR -> {
                         this.setTileType(TileType.WORKER_IN_STORAGE_AREA);
-                        this.setAdjacentCellContent(direction, TileType.UNSTORED_BOX);
+                        this.setAdjacentCellType(direction, TileType.UNSTORED_BOX);
                     }
                     default -> {
                     }
@@ -93,13 +93,13 @@ public class Cell {
             }
         }
     	Direction previous_player = direction.reverse();
-        return switch (this.getAdjacentCellContent(previous_player)) {
+        return switch (this.getAdjacentCellType(previous_player)) {
             case WORKER_ON_FLOOR -> {
-                this.setAdjacentCellContent(previous_player, TileType.FLOOR);
+                this.setAdjacentCellType(previous_player, TileType.FLOOR);
                 yield true;
             }
             case WORKER_IN_STORAGE_AREA -> {
-                this.setAdjacentCellContent(previous_player, TileType.STORAGE_AREA);
+                this.setAdjacentCellType(previous_player, TileType.STORAGE_AREA);
                 yield true;
             }
             default -> false;
